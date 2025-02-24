4d3b45f9dee6081d494be7abce0440e6a9d2bf30
NoobScience <noobscience123@gmail.com>
2025-02-24 23:31:05 +0530

    feat: Downgrade elixir requirements

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule WaSomeBack.MixProject do
     [
       app: :wa_some_back,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
